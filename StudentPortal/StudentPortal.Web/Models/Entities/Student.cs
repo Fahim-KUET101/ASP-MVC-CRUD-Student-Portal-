56bea1150e64f9ee8255932d86bd2282d76eb325
USER <USER@DESKTOP-LU2M1HS>
2026-03-07 00:59:06 +0600

    Add project files.

---
+++ StudentPortal/StudentPortal.Web/Models/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentPortal.Web.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public bool Subscribes { get; set; }
+    }
+}
